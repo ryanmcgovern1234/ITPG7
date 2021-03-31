9bc96429810967cd7061b8568386a75cc9f8533e
Alikhan <alikhan.sabyruly@student.manchester.ac.uk>
2021-04-01 02:56:54 +0600

    a

---
+++ app/src/main/java/com/example/itpg7/ui/MapPage.java
@@ -0,0 +1,16 @@
+package com.example.itpg7.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itpg7.R;
+
+public class MapPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
