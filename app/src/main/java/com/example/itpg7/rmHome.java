d7041d4e6496c57b598456178415e63248486a22
toadersasa <68132327+toadersasa@users.noreply.github.com>
2021-04-19 01:33:59 +0100

    Added RM Home with some edits

--- app/src/main/java/com/example/itpg7/rmHome.java
+++ app/src/main/java/com/example/itpg7/rmHome.java
@@ -1,5 +1,6 @@
 package com.example.itpg7;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,12 +9,14 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.itpg7.HelperClasses.HomeAdapter.FeaturedAdapter;
 import com.example.itpg7.HelperClasses.HomeAdapter.FeaturedHelperClass;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -32,9 +35,35 @@ public class rmHome extends AppCompatActivity{
 
         //hooks
         newsRecycler = findViewById(R.id.news_recycler);
-
         newsRecycler();
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+
+        //Set Selected
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        //Listener to give functionality
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_home:
+                        startActivity(new Intent(getApplicationContext(), rmHome.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.navigation_clientoverview:
+                        startActivity(new Intent(getApplicationContext(), ClientOverviewPage.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.navigation_dashboard:
+                        startActivity(new Intent(getApplicationContext(), MarketView.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
         
         
         
